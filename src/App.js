0c9b4fbc222fe18ea70699a616d45a6a1dd674cd
Ariel <ariel.fuentesgarcia@gmail.com>
2022-10-22 14:23:05 -0500

    chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Chart from './Chart';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/data", {method: "GET", redirect: "follow"})
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json);
+      setData(json);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Chart</h1>
+      <Chart labels={data.length === 0 ? ["pink"] : data[0].labels} 
+      data1={data.length === 0 ? [0,0,0,0,0,0,0] : data[0].data[0].values}
+      data2={data.length === 0 ? [0,0,0,0,0,0,0] : data[0].data[1].values} 
+      />
     </div>
   );
 }
